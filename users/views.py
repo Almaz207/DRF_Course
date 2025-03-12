1efbb2e7a27c1b4229bcf195dd73eec69c285f28
Almaz207 <128742839+Almaz207@users.noreply.github.com>
2025-03-12 08:38:53 +0400

    Update views.py
    add CRUD mechenicals for Payments, but must make "urls" for Payments and "filters" for him

--- users/views.py
+++ users/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+    DestroyAPIView,
+)
 
-# Create your views here.
+from materials.models import Payments
+
+from users.serializers import PaymentsSerializer
+
+class PaymentsCreateApiView(CreateAPIView):
+    queryset = Payments.objects.all()
+    serializer_class = PaymentsSerializer
+
+
+class PaymentsListApiView(ListAPIView):
+    queryset = Payments.objects.all()
+    serializer_class = PaymentsSerializer
+
+
+class PaymentsRetrieveApiView(RetrieveAPIView):
+    queryset = Payments.objects.all()
+    serializer_class = PaymentsSerializer
+
+
+class PaymentsUpdateApiView(UpdateAPIView):
+    queryset = Payments.objects.all()
+    serializer_class = PaymentsSerializer
+
+
+class PaymentsDestroyApiView(DestroyAPIView):
+    queryset = Payments.objects.all()
+    serializer_class = PaymentsSerializer
