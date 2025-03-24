92d1b9f12b70245166f5121953f908d3caa32e9e
Almaz207 <muzafarov.almaz2012@yandex.ru>
2025-03-25 00:08:44 +0400

    add settings celery and redis

--- users/tasks.py
+++ users/tasks.py
@@ -1,5 +1,6 @@
 from celery import shared_task
-from datetime import timedelta
+
+from dateutil.relativedelta import relativedelta
 from django.utils import timezone
 
 from users.models import CustomUser
@@ -8,12 +9,6 @@ from users.models import CustomUser
 @shared_task
 def check_last_login():
     """Проверка последнего входа пользователей и отключение неактивных пользователей"""
-    users = CustomUser.objects.filter(last_login__isnull=False)
-    today = timezone.now()
-    for user in users:
-        if today - user.last_login > timedelta(days=30):
-            user.is_active = False
-            user.save()
-            print(f'Пользователь {user.email} отключен')
-        else:
-            print(f'Пользователь {user.email} активен')
+    month_ago = timezone.now() - relativedelta(months=1)
+    users_to_block = CustomUser.objects.filter(is_active=True, last_login__lte=month_ago)
+    users_to_block.update(is_active=False)
